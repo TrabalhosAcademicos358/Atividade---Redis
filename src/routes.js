cd3bb971a5980edba3723ef23bda92dfe281f329
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-11-28 18:52:21 -0300

    Adicionando função router no arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,9 @@ const cacheControl = require("./controller/CacheController")
 
 router.post("/pessoa", pessoaControl.postPessoa)
 
-router.get("/pessoa/:id", cacheControl.getPessoa, pessoaControl.getPessoa, cacheControl.setPessoa)
-
-router.put("/pessoa/:id", pessoaControl.putPessoa, cacheControl.setPessoa)
-
-router.delete("/pessoa/:id", pessoaControl.delPessoa, cacheControl.delPessoa)
+router.route("/pessoa/:id")
+    .get(cacheControl.getPessoa, pessoaControl.getPessoa, cacheControl.setPessoa)
+    .put(pessoaControl.putPessoa, cacheControl.setPessoa)
+    .delete(pessoaControl.delPessoa, cacheControl.delPessoa)
 
 module.exports = router
